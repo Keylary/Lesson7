f00ee07e03d150a907f137a3f1d1d3a515f556c0
Keylary <80574364+Keylary@users.noreply.github.com>
2021-03-26 16:40:46 +0300

    1.1

---
+++ Lesson7/Lesson7.cpp
@@ -0,0 +1,10 @@
+﻿// Lesson7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
